fe28743468650afa48ea9cf40f0d3b531ea9cbdc
minalz <119687281@qq.com>
2021-04-30 00:40:57 +0800

    添加mybatis-testcase的测试项目，方便调试mybatis的源码

---
+++ src/main/java/com/minalz/mapper/BlogMapperExt.java
@@ -0,0 +1,17 @@
+package com.minalz.mapper;
+
+import com.minalz.domain.Blog;
+
+/**
+ *
+ * 扩展类继承了MBG生成的接口和Statement
+ * @Author: minalz
+ */
+public interface BlogMapperExt extends BlogMapper {
+    /**
+     * 根据名称查询文章
+     * @param name
+     * @return
+     */
+    public Blog selectBlogByName(String name);
+}
---
+++ src/main/java/com/minalz/objectfactory/GPObjectFactory.java
@@ -0,0 +1,26 @@
+package com.minalz.objectfactory;
+
+import com.minalz.domain.Blog;
+import org.apache.ibatis.reflection.factory.DefaultObjectFactory;
+
+/**
+ * @Author: minalz
+ * <p>
+ * 自定义ObjectFactory，通过反射的方式实例化对象
+ * 一种是无参构造函数，一种是有参构造函数——第一个方法调用了第二个方法
+ */
+public class GPObjectFactory extends DefaultObjectFactory {
+    @Override
+    public Object create(Class type) {
+        System.out.println("创建对象方法：" + type);
+        if (type.equals(Blog.class)) {
+            Blog blog = (Blog) super.create(type);
+            blog.setName("object factory");
+            blog.setBid(1111);
+            blog.setAuthorId(2222);
+            return blog;
+        }
+        Object result = super.create(type);
+        return result;
+    }
+}
