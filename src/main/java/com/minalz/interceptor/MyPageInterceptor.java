dc26ae3d0a8801ef71e2481672aec8d7dd33a870
minalz <119687281@qq.com>
2021-05-18 09:40:20 +0800

    添加插件相关的源码注释

--- src/main/java/com/minalz/interceptor/MyPageInterceptor.java
+++ src/main/java/com/minalz/interceptor/MyPageInterceptor.java
@@ -12,6 +12,21 @@ import java.lang.reflect.Field;
 import java.util.Properties;
 /**
  * @Author: minalz
+ * 编写拦截器：三个步骤
+ * 1.实现Interceptor接口
+ * 2.实现响应的方法，最关键的是intercept()方法，里面是拦截的逻辑，需要增强的功能代码就写在这里
+ * 3.在拦截器类上加上朱姐，注解签名指定了需要拦截的对象、拦截的方法、参数(因为方法有不同的重载，所以要指定具体的参数)
+ * 原理：
+ * 代理模式+责任链模式
+ * 可以进行拦截的四个对象：
+ * 1.Executor 上层的对象，SQL执行全过程，包括组装参数，组装结果集返回和执行SQL过程
+ * --> 因为Executor有可能被二级缓存装饰，那么是先代理再装饰，还是先装饰再代理呢？
+ * Executor会先拦截到CachingExecutor或者BaseExecutor
+ * DefaultSqlSessionFactory.openSessionFromDataSource():
+ * 看执行步骤后得知：先创建基本类型，再二级缓存装饰，最后插件拦截。所以这里拦截的是CachingExecutor.
+ * 2.StatementHandler 执行SQL的过程，最常用的拦截对象
+ * 3.ParameterHandler SQL参数组装的过程
+ * 4.ResultSetHandler 结果集的组装
  */
 @Intercepts({@Signature(type = Executor.class,method = "query",
         args = {MappedStatement.class, Object.class, RowBounds.class, ResultHandler.class})
